47610045b2e7c630d0f9d42e0dab13dc8e6d21ee
mairo <miroslav.mikovec@gmail.com>
2022-12-01 16:13:50 +0100

    Initial Commit

---
+++ OnTheMapV.1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OnTheMapV.1
+//
+//  Created by mairo on 01/12/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
